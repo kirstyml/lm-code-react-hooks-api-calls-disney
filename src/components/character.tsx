368301d025b743574a11f48be8554b22c8b9b38c
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-04 10:24:01 +0000

    Added default image and imageUrl handling to Character

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -2,8 +2,16 @@ import { DisneyCharacter } from "../disney_character"
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
-  
+const Character: React.FC<{ character: DisneyCharacter }> = ({ character }) => {
+  // Define a default in case the character doesn't have an image
+  let imageSrc = "https://picsum.photos/300/200/?blur";
+  if (character.imageUrl) {
+    // API seems to include extra path for images so here we strip it off to fetch raw image
+    const indexOfExtraPath = character.imageUrl.indexOf('/revision');
+    imageSrc = indexOfExtraPath >= 0 ? character.imageUrl.substring(0, indexOfExtraPath) : character.imageUrl;
+  }
+
+  return (
     <article className="character-item">
 
       <h2>{character.name}</h2>
@@ -12,10 +20,10 @@ const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) =>
         Add to Favourites
       </div>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+      <img className="character-item__img" src={imageSrc} alt={character.name} />
 
     </article>
-  
-
+  )
+}
 
 export default Character
