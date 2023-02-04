14c1e53b1f4489012579144457d0bc137fd0d31a
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-02-04 10:29:07 +0000

    Updated useEffect in App to call api for current page number

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ const App : React.FC = () => {
   const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
 
   useEffect(() => {
-    getCharacters(1);
-  }, []);
+    getCharacters(currentPage);
+  }, [currentPage]);
 
   const getCharacters = async (pageNumber : number) => {
     const apiResponse = await fetch(`http://api.disneyapi.dev/characters?page=${pageNumber}`);
